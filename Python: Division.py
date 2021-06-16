6407ff944008e21fd80a6ff2114b7475062bc728
SaiSree-0517 <69571289+SaiSree-0517@users.noreply.github.com>
2021-06-16 19:52:37 +0530

    Create Python: Division.py

---
+++ Python: Division.py
@@ -0,0 +1,14 @@
+#Python: Division
+if __name__ == '__main__':
+    a = int(input())
+    b = int(input())
+    print(a//b)
+    print(a/b)
+ """
+ Input (stdin)
+4
+3
+Expected Output
+1
+1.33333333333
+"""
