edce028f503b08defde97cd3f9f330217e4effc4
SaiSree-0517 <69571289+SaiSree-0517@users.noreply.github.com>
2021-06-16 19:51:20 +0530

    Create Arithmetic Operators.py

---
+++ Arithmetic Operators.py
@@ -0,0 +1,16 @@
+#Arithmetic Operators
+if __name__ == '__main__':
+    a = int(input())
+    b = int(input())
+    print(a+b)
+    print(a-b)
+    print(a*b)
+"""
+Input (stdin)
+3
+2
+Expected Output
+5
+1
+6
+"""
